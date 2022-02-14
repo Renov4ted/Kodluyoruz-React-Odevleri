d30beae7f5b8fc4fa3af4d8921ce4c032e4d2811
Renov4ted <74363444+Renov4ted@users.noreply.github.com>
2022-02-15 02:39:37 +0300

    React Odev-1

---
+++ React Odev-1/data.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const info = (id) =>{
+   return new Promise(async(resolve, reject)=>{
+        const {data} = await axios ("https://jsonplaceholder.typicode.com/users/" +id)
+        resolve(data)
+    })
+}
+
+const post = (id) =>{
+     return new Promise (async(resolve, reject) =>{
+        const {data} = await axios ("https://jsonplaceholder.typicode.com/posts/" + id)
+        resolve(data)
+        
+    })
+}   
+(async()=>
+{
+await info(1)
+.then((data)=> console.log(data))
+.catch((e)=> console.log(e))
+
+await post(1)
+.then((data)=>console.log(data))
+.catch((e)=>console.log(e))
+})()
+
+export default info
+
+
+// Promise.all([info(1),post(1)]).then (console.log).catch(console.log)
